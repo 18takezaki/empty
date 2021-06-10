4db68eea18da10567e107d193ff469577acf1b0b
kensiro2248@gmail.com <kensiro2248@gmail.com>
2021-06-10 11:35:29 +0900

    OSERO

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -27,14 +27,14 @@ enum {
 	DIRECTION_MAX
 };
 int directions[][2] = {
-	{},// DIRECTION_UP
-	{},// DIRECTION_UP_LEFT
-	{},// DIRECTION_LEFT
-	{},// DIRECTION_DOWN_LEFT
-	{},// DIRECTION_DOWN
-	{},// DIRECTION_DOWN_RIGTH
-	{},// DIRECTION_RIGHT
-	{},// DIRECTION_UP_RIGHT
+	{0,-1},// DIRECTION_UP
+	{-1,-1},// DIRECTION_UP_LEFT
+	{-1,0},// DIRECTION_LEFT
+	{-1,1},// DIRECTION_DOWN_LEFT
+	{0,1},// DIRECTION_DOWN
+	{1,1},// DIRECTION_DOWN_RIGTH
+	{1,0},// DIRECTION_RIGHT
+	{1,-1},// DIRECTION_UP_RIGHT
 };
 char colorNames[][5 + 1] = {
 	"Black",
@@ -46,12 +46,71 @@ int cells[BOARD_HEIGHT][BOARD_WIDTH];
 int cursorX, cursorY;
 int turn;
 
-bool checkCanPut(int color, int _x, int _y) {
-	if(cells[_y][_x]!=COLOR_NONE)
+bool checkCanPut(int _color, int _x, int _y,bool _turnOver) {
+	if(cells[_y][_x] != COLOR_NONE)
 		return false;
 
-	return true;
+	for (int i = 0; i < DIRECTION_MAX; i++) {
+		int x = _x, y = _y;
+		x += directions[i][0];
+		y += directions[i][1];
+		if (cells[y][x] != (_color ^ 1))
+			continue;
+		while (1) {
+			x += directions[i][0];
+			y += directions[i][1];
 
+			if ((x < 0) || (x >= BOARD_WIDTH) || (y < 0) || (y >= BOARD_HEIGHT))
+				break;
+			if (cells[y][x] == COLOR_NONE)
+				break;
+			if (cells[y][x] == _color) {
+				if(!_turnOver)
+					return true;
+
+				int x2 = _x, y2 = _y;
+				while(1){
+					cells[y2][x2] = _color;
+
+					x2 += directions[i][0];
+					y2 += directions[i][1];
+
+					if ((x2 == x) && (y2 == y))
+						break;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+/*bool checkCanPutAll(int _color) {
+	for (int y = 0; y < BOARD_HEIGHT; y++) {
+		for (int x = 0; x < BOARD_WIDTH; x++)
+			if (checkCanPut(_color, x, y, false))
+				return true;
+
+		return false;
+	}
+
+
+
+}*/
+void drowBoard() {
+	system("cls");
+	for (int y = 0; y < BOARD_HEIGHT; y++) {
+		for (int x = 0; x < BOARD_WIDTH; x++)
+			if ((x == cursorX) && (y == cursorY))
+				printf("");
+			else {
+				switch (cells[y][x]) {
+				case COLOR_NONE: printf("E"); break;
+				case COLOR_BLACK:printf("Z"); break;
+				case COLOR_WHITE:printf("œ"); break;
+				}
+			}
+		printf("\n");
+	}
 }
 int main() {
 	for (int y = 0; y < BOARD_HEIGHT; y++) {
@@ -62,23 +121,11 @@ int main() {
 	cells[4][3] = cells[3][4] = COLOR_BLACK;
 	bool cantPut = false;
 	while (1) {
-		system("cls");
-		for (int y = 0; y < BOARD_HEIGHT; y++) {
-			for (int x = 0; x < BOARD_WIDTH; x++)
-				if ((x == cursorX) && (y == cursorY))
-					printf("");
-				else {
-					switch (cells[y][x]) {
-					case COLOR_NONE: printf("E"); break;
-					case COLOR_BLACK:printf("Z"); break;
-					case COLOR_WHITE:printf("œ"); break;
-					}
-				}
-			printf("\n");
-		}
+		drowBoard();
+		
 		if (cantPut)
 			printf("Can't put!\n");
-		else 
+		else
 			printf("%s turn \n", colorNames[turn]);
 
 		cantPut = false;
@@ -88,15 +135,46 @@ int main() {
 		case'a': cursorX--; break;
 		case'd': cursorX++; break;
 		default:
-			if (!checkCanPut(turn, cursorX, cursorY)) {
+			if (!checkCanPut(turn, cursorX, cursorY, false)) {
 				cantPut = true;
 				break;
 			}
 
+			checkCanPut(turn, cursorX, cursorY, true);
+
 			cells[cursorY][cursorX] = turn;
 			turn ^= 1;	//‚P‚¾‚Á‚½‚ç‚OA‚O‚¾‚Á‚½‚ç‚P‚É•ÏŠ·
+
+			/*if (!checkCanPutAll(turn))
+				turn ^= 1;*/
+			
 			break;
 		}
+		/*if ((!checkCanPutAll(COLOR_BLACK)) && (!checkCanPutAll(COLOR_WHITE))) {
+			int count[COLOR_MAX] = {};
+			for (int y = 0; y < BOARD_HEIGHT; y++) 
+				for (int x = 0; x < BOARD_WIDTH; x++)
+					if (cells[y][x] != COLOR_NONE)
+						count[cells[y][x]]++;
+
+			drowBoard();
+			for (int i = 0; i < COLOR_MAX; i++)
+				printf("%s:%d\n", colorNames[i], count[i]);
+
+			if (count[COLOR_BLACK] == count[COLOR_WHITE])
+				printf("Doraw!\n");
+			    
+			else {
+				if (count[COLOR_BLACK] > count[COLOR_WHITE])
+					printf("%s\n", colorNames[COLOR_BLACK]);
+				else printf("%s\n", colorNames[COLOR_WHITE]);
+
+				printf(" Won1\n");
+			}
+
+			_getch();
+			break;
+		}*/
 	}
 }
 #endif
