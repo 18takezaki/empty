a7dc4316899ed50c35048ebf16dde5ded08f7c58
kensiro2248@gmail.com <kensiro2248@gmail.com>
2021-06-17 12:51:01 +0900

    HOSHI,TAKAKUKE,CUSTOM

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,4 +1,117 @@
-#define _ACTION
+#define _HOSHI
+
+#ifdef _HOSHI
+#include "libOne.h"
+
+int createTriangle() {
+	struct SHAPE_VERTEX vertices[3];
+	vertices[0].x = 0;
+	vertices[0].y = -1;
+	vertices[1].x = 1;
+	vertices[1].y = 1;
+	vertices[2].x = -1;
+	vertices[2].y = 1;
+	return createShape(vertices, 3);
+}
+int createPolygon() {
+	const int NUM = 6;
+	struct SHAPE_VERTEX vertices[NUM];
+	float deg = 360.0f / NUM;
+	angleMode(DEGREES);
+	for (int i = 0; i < NUM; i++) {
+		vertices[i].x = sin(deg * i);
+		vertices[i].y = -cos(deg * i);
+	}
+	return createShape(vertices, NUM);
+}
+
+int createStar() {
+	const int NUM = 10;
+	struct SHAPE_VERTEX vertices[NUM];
+	float deg = 360.0f / NUM;
+	angleMode(DEGREES);
+	for (int i = 0; i < NUM; i++) {
+		float radius = 0.5f + 0.5f * (i % 2);
+		vertices[i].x = sin(deg * i)*radius;
+		vertices[i].y = cos(deg * i)*radius;
+	}
+	return createShape(vertices, NUM);
+}
+void gmain() {
+	window(1000, 1000);
+	int idx = createStar();
+	//int idx = ();
+	//int idx = createPolygon();
+	float deg = 0;
+	angleMode(DEGREES);
+	while (notQuit) {
+		deg += 1;
+		clear(200);
+		strokeWeight(5);
+		fill(255, 255, 0);
+		shape(idx, 100, 500, deg, 100);
+	}
+}
+#endif
+
+#ifdef _TAKAKUKE
+#include "libOne.h"
+
+int createPolygon() {
+	const int NUM = 6;
+	struct SHAPE_VERTEX vertices[NUM];
+	float deg = 360.0f / NUM;
+	angleMode(DEGREES);
+	for (int i = 0; i < NUM; i++) {
+		vertices[i].x = sin(deg*i);
+		vertices[i].y = -cos(deg*i);
+	}
+	return createShape(vertices, NUM);
+}
+void gmain() {
+	window(1000, 1000);
+	int idx = createPolygon();
+	float deg = 0;
+	angleMode(DEGREES);
+	while (notQuit) {
+		deg += 1;
+		clear(200);
+		strokeWeight(5);
+		fill(255, 255, 0);
+		shape(idx, width / 2, height / 2, deg, 200);
+	}
+}
+#endif
+
+#ifdef _CUSTOM
+#include "libOne.h"
+int createTriangle() {
+	struct SHAPE_VERTEX vertices[3];
+	vertices[0].x = 0;
+	vertices[0].y = -1;
+	vertices[1].x = 1;
+	vertices[1].y = 1;
+	vertices[2].x = -1;
+	vertices[2].y = 1;
+	return createShape(vertices, 3);
+}
+void gmain() {
+	window(1000, 1000);
+	int idx = createTriangle();
+	float deg = 0;
+	angleMode(DEGREES);
+	while (notQuit) {
+		deg += 1;
+		clear(200);
+		strokeWeight(5);
+		fill(255, 255, 0);
+		shape(idx, width/2, height/2, deg, 200);
+	}
+}
+#endif
+
+#ifdef _JIKAN 
+#endif
 
 #ifdef _ACTION
 #include"libOne.h"
