c3987773ba3de033891bbd095d9ffb86ce4f1171
kensiro2248@gmail.com <kensiro2248@gmail.com>
2021-06-22 15:50:37 +0900

    VECTOR

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,4 +1,27 @@
-#define _STAR
+#define _VECTOR
+
+#ifdef _VECTOR
+#include"libOne.h"
+void gmain() {
+	window(1000, 1000);
+	while (notQuit) {
+		float vx = mathMouseX;
+		float mag = vx < 0 ? -vx : vx;
+		float nvx = vx / mag;
+		clear(200);
+		mathAxis(5.1f);
+		strokeWeight(10);
+		stroke(0);
+		mathArrow(0, 0, vx, 0);
+		stroke(255, 0, 0);
+		mathArrow(0, 0, nvx, 0);
+
+		textSize(50);
+		text(vx, 0, 50);
+		text(mag, 0, 100);
+	}
+}
+#endif
 
 #ifdef _STAR
 #include"libOne.h"
