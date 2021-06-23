f1216089ba6c0ab6d28776f49b285a658aa753dd
kensiro2248@gmail.com <kensiro2248@gmail.com>
2021-06-23 16:38:51 +0900

    VEC2

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,24 +1,82 @@
-#define _VECTOR
+#define _VEC2
+
+#ifdef _VEC2
+#include"libOne.h"
+void gmain() {
+	window(1000, 1000);
+	float rx = 5, ry = 5;
+	float bx = 7, by = 7;
+	while (notQuit) {
+		bx = mathMouseX;
+		by = mathMouseY;
+		float vx = bx - rx;
+		float vy = by - ry;
+		float mag = sqrt(vx * vx + vy * vy);
+		float nvx = vx / mag;
+		float nvy = vy / mag;
+		rx += nvx * 0.01f;
+		ry += nvy * 0.01f;
+		clear(200);
+		mathAxis(9.1f);
+		strokeWeight(5);
+		stroke(0);
+		mathArrow(0, 0, vx, vy);
+		mathLine(0, 0, vx, 0);
+		mathLine(vx, 0, vx, vy);
+		stroke(255, 0, 0);
+		mathArrow(0, 0, nvx, nvy);
+		mathLine(0, 0, nvx, 0);
+		mathLine(nvx, 0, nvx, nvy);
+		
+		strokeWeight(20);
+		stroke(255, 0, 0);
+		mathPoint(rx, ry);
+		stroke(0, 0, 255);
+		mathPoint(bx, by);
+
+		textSize(50);
+		text((let)"vec(" + vx + "," + vy + ")", 0, 50);
+		text((let)"mag:" + mag, 0, 100);
+		textSize(50);
+		text((let)"nvec(" + nvx + "," + nvy + ")", 0, 150);
+		mag = sqrt(nvx * nvx + nvy * nvy);
+		text((let)"nmag:" + mag, 0, 200);
+	}
+}
+#endif
 
 #ifdef _VECTOR
 #include"libOne.h"
 void gmain() {
 	window(1000, 1000);
+	float rx = 5;
+	float bx = 7;
 	while (notQuit) {
-		float vx = mathMouseX;
+		float vx = rx - bx;
 		float mag = vx < 0 ? -vx : vx;
 		float nvx = vx / mag;
+		bx += nvx * 0.001f;
 		clear(200);
-		mathAxis(5.1f);
+		mathAxis(9.1f);
 		strokeWeight(10);
 		stroke(0);
 		mathArrow(0, 0, vx, 0);
 		stroke(255, 0, 0);
 		mathArrow(0, 0, nvx, 0);
 
+		strokeWeight(20);
+		stroke(255, 0, 0);
+		mathPoint(rx, 0);
+
+		strokeWeight(20);
+		stroke(0, 0, 255);
+		mathPoint(bx, 0);
+
 		textSize(50);
 		text(vx, 0, 50);
 		text(mag, 0, 100);
+
+		 
 	}
 }
 #endif
