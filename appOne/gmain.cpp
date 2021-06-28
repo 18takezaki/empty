2c96074954f2890c149706b3d11a368fc1f646f5
kensiro2248@gmail.com <kensiro2248@gmail.com>
2021-06-28 11:21:45 +0900

    tetoris

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,5 +1,208 @@
-#define _VEC2
+#define _TETORIS
 
+#ifdef _TETORIS
+#include"libOne.h"
+const int INIT = 0;
+const int PLAY = 1;
+const int OVER = 2;
+int GameState = INIT;
+float Size = 40;
+const int ROWS = 26;
+const int COLS = 12;
+float Satu = 255;
+float Value = 255;
+float Alpha = 255;
+int FallFlag = 0;
+struct COLOR Color[9] = {
+	COLOR(0,Satu,Value,Alpha),
+	COLOR(30,Satu,Value,Alpha),
+	COLOR(60,Satu,Value,Alpha),
+	COLOR(120,Satu,Value,Alpha),
+	COLOR(180,Satu,Value,Alpha),
+	COLOR(240,Satu,Value,Alpha),
+	COLOR(300,Satu,Value,Alpha),
+	COLOR(200,44,88,Alpha),
+	COLOR(0,0,20,Alpha)
+};
+const int WALL = 7;
+const int BACK = 8;
+int Stage[ROWS][COLS];
+int X, Y, R;
+int Px[4], Py[4];
+int PtnNo = 0;
+int Ptn[7][3][2] = {
+	//■□■■...0
+	-1,0,   1,0,  2,0,
+	//■
+	//■□■....1
+	-1,-1, -1,0,  1,0,
+	//  　■
+	//■□■....2
+	-1,0,   1,-1, 1,0,
+	//■□
+	//　■■....3
+	-1,0,   0,1,  1,1,
+	//　□■
+	//■■  ....4
+	 1,0,   0,1, -1,1,
+	 //　■
+	 //■□■....5
+	 -1,0,   0,-1, 1,0,
+	 //□■
+	 //■■......6
+	  1,0,   0,1,  1,1,
+};
+int LoopCnt = 0;
+
+void init() {
+	for (int y = 0; y < ROWS; y++) {
+		Stage[y][0] = WALL;
+		Stage[y][COLS - 1] = WALL;
+		for (int x = 1; x < COLS - 1; x++) {
+			Stage[y][x] = BACK;
+			if (y == ROWS - 1) {
+				Stage[y][x] = WALL;
+			}
+		}
+	}
+	X = 5;
+	Y = 1;
+	R = 0;
+	PtnNo = random() % 7;
+	GameState = PLAY;
+}
+void drawStage() {
+	colorMode(HSV);
+	angleMode(DEGREES);
+	for (int y = 0; y < ROWS; y++) {
+		for (int x = 0; x < COLS; x++) {
+			int no = Stage[y][x];
+			fill(Color[no]);
+			rect(Size * x, Size * y, Size, Size);
+		}
+	}
+}
+void setPosition() {
+	Px[0] = X;
+	Py[0] = Y;
+	int r = R % 4;
+	for (int i = 0; i < 3; i++) {
+		int x = Ptn[PtnNo][i][0];
+		int y = Ptn[PtnNo][i][1];
+		for (int j = 0; j < r; j++) {
+			int w = x;
+			x = y;
+			y = -w;
+		}
+		Px[i + 1] = X + x;
+		Py[i + 1] = Y + y;
+	}
+}
+void setPtn() {
+	setPosition();
+	for (int i = 0; i < 4; i++) {
+		int x = Px[i];
+		int y = Py[i];
+		Stage[y][x] = PtnNo;
+	}
+}
+void delPtn() {
+	setPosition();
+	for (int i = 0; i < 4; i++) {
+		int x = Px[i];
+		int y = Py[i];
+		Stage[y][x] = BACK;
+	}
+}
+int collistion() {
+	setPosition();
+	int flag = 0;
+	for (int i = 0; i < 4; i++) {
+		int x = Px[i];
+		int y = Py[i];
+		if (Stage[y][x] != BACK) {
+			flag = 1;
+		}
+	}
+	return flag;
+}
+void complete() {
+	for (int y = 1; y < ROWS - 1; y++) {
+		int flag = 1;
+		for (int x = 1; x < COLS - 1; x++) {
+			if (Stage[y][x] == BACK) {
+				flag = 0;
+			}
+
+		}
+		if (flag) {
+			for (int upy = y - 1; upy > 0; upy--) {
+				for (int x = 1; x < COLS - 1; x++) {
+					Stage[upy + 1][x] = Stage[upy][x];
+				}
+			}
+		}
+	}
+}
+void play() {
+	//Stage[Y][X] = BACK;
+	delPtn();
+
+	int dy = 0, dx = 0, dr = 0;
+	if ((++LoopCnt %= 10) == 0)dy = 1;
+	if (isTrigger(KEY_D))dx = 1;
+	if (isTrigger(KEY_A))dx = -1;
+	if (isTrigger(KEY_W))dr = 1;
+	if (isTrigger(KEY_S))FallFlag = 1;
+	if (FallFlag)dy = 1;
+	X += dx;
+	Y += dy;
+	R += dr;
+	if (collistion()) {
+		Y -= dy;
+		X -= dx;
+		R -= dr;
+		FallFlag = 0;
+		if (dy == 1 && dx == 0 && dr == 0) {
+			//Stage[Y][X] = PtnNo;
+			setPtn();
+			complete();
+			//新しいブロック
+			Y = 1;
+			X = 5;
+			R = 0;
+			PtnNo = random() % 7;
+			if (collistion()) {
+				GameState = OVER;
+			}
+		}
+	}
+	//Stage[Y][X] = PtnNo;
+	setPtn();
+	clear(0);
+	drawStage();
+}
+void over() {
+	clear(0);
+	drawStage();
+	fill(0, 0, 255);
+	textSize(Size);
+	text("game over", Size * 2, Size);
+
+	if (isTrigger(KEY_R)) {
+		GameState = INIT;
+	}
+}
+void gmain() {
+	window(Size * COLS, Size * ROWS);
+	while (notQuit) {
+		if (GameState == INIT)init();
+		else if (GameState == PLAY)play();
+		else if (GameState == OVER)over();
+	}
+}
+
+#endif
 #ifdef _VEC2
 #include"libOne.h"
 void gmain() {
